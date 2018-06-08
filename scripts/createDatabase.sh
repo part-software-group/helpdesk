d492d29ad367cf96b53fc4e246db6a61e632545e
pooya <pooya_azarpour@yahoo.com>
2018-06-09 00:09:14 +0430

    Delete old file

--- scripts/createDatabase.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-psql -U postgres postgres -c "CREATE DATABASE $(sed -rn  's/database.+\/(.+)/\1/p' /tmp/config)"
-
-psql -U postgres $(sed -rn  's/database.+\/(.+)/\1/p' /tmp/config) -f /tmp/postgresql.sql
